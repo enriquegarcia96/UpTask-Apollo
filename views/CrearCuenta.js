dc1ee221edd334064e72ab9da6821340df9aa381
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-29 15:52:40 -0600

    creando la pantalla de crear cuenta para el usuario

---
+++ views/CrearCuenta.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+
+const CrearCuenta = () => {
+    return (
+        <Text>Crear cuenta</Text>
+    )
+}
+
+export default CrearCuenta

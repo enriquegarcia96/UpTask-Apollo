944ef908c366d6d7d92afd32acb0053a635a4eea
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-04-29 20:53:41 -0600

    Inputs, texto y estilos para la pantalla del Login

--- views/Login.js
+++ views/Login.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View } from 'react-native'
+
+import { Container, Button, Text, H1, Input, Form, Item, Toast } from 'native-base';
+import globalStyles from '../styles/global';
 
 
 const Login = () => {
     return (
-        <Text>Login</Text>
+        <Container style={[{ backgroundColor: '#E84347'}, globalStyles.contenedor ] }>
+            <View style={ globalStyles.contenido }>
+                <H1 style={ globalStyles.titulo } >Uptask</H1>
+
+                <Form>
+                    <Item inlineLabel last style={ globalStyles.input } >
+                        <Input 
+                            placeholder='Email'
+                        />
+                    </Item>
+
+                    <Item inlineLabel last style={ globalStyles.input } >
+                        <Input 
+                            secureTextEntry={true}
+                            placeholder='Password'
+                        />
+                    </Item>
+                </Form>
+
+                <Button
+                    square
+                    block
+                    style={ globalStyles.boton }
+                >
+                    <Text
+                        style={ globalStyles.botonTexto }
+                    >Iniciar Sesión</Text>
+                </Button>
+
+                <Text style={ globalStyles.enlace } >Crear Cuenta</Text>
+
+            </View>
+        </Container>
     )
 }
 

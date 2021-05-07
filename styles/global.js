dad3ba465acb95f71fcd3e9bb9fe1292c9bdd12d
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-05-06 18:37:37 -0600

    estilos globales de la APP

--- styles/global.js
+++ styles/global.js
@@ -15,12 +15,21 @@ const globalStyles = StyleSheet.create({
         textAlign: 'center',
         marginBottom: 20,
         fontSize: 32,
-        fontWeight:'bold',
+        fontFamily:'Girassol-Regular' ,
         color: '#FFF'
+    },
+    subtitulo:{
+        textAlign: 'center',
+        marginBottom: 20,
+        fontSize: 26,
+        fontWeight:'bold',
+        color: '#FFF',
+        marginTop: 20
     },
     input:{
         backgroundColor: '#FFF',
         marginBottom: 20,
+        fontFamily: 'inputs'
 
     },
     boton:{
@@ -39,6 +48,13 @@ const globalStyles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 18,
         textTransform: 'uppercase'
+    },
+    firma:{
+        textAlign: 'center',
+        marginBottom: 20,
+        fontSize: 32,
+        fontFamily:'Montserrat-ThinItalic' ,
+        color: '#FFF'
     }
 
 });
